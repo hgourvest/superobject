30fdf0aa153a498944f707667625532bf1e1ab8f
Wouter van Nifterick <woutervannifterick@gmail.com>
2019-01-06 21:43:14 +0100

    Use more future-proof version checking

--- superobject.pas
+++ superobject.pas
@@ -83,13 +83,7 @@
 {$DEFINE SUPER_METHOD}
 {.$DEFINE DEBUG} // track memory leak
 
-
-{$if defined(VER210) or defined(VER220)}
-  {$define VER210ORGREATER}
-{$ifend}
-
-{$if defined(VER230) or defined(VER240)  or defined(VER250) or
-     defined(VER260) or defined(VER270)  or defined(VER280)}
+{$if CompilerVersion > 20}
   {$define VER210ORGREATER}
   {$define VER230ORGREATER}
 {$ifend}
